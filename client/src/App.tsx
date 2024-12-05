4a94a66c189890c4fcbdb0079045e20ff375b37b
Keith <keith.amadeus.williams@gmail.com>
2024-12-05 14:34:58 -0700

    added apollo requests to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+// Create an Apollo Provider to make every request work with the Apollo Server.
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
