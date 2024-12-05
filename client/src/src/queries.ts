8441e20b12b6aecadca2f98c6ac1cf51c45e593e
Keith <keith.amadeus.williams@gmail.com>
2024-12-05 14:20:55 -0700

    added code to queries that matched back-end process

--- client/src/src/queries.ts
+++ client/src/src/queries.ts
@@ -1,24 +1,43 @@
 import { gql } from '@apollo/client';
 // This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
 
-
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+export const GET_ME = gql`
+  query me {
+    me {
       _id
-      name
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
+
+// export const QUERY_TECH = gql`
+//   query tech {
+//     tech {
+//       _id
+//       name
+//     }
+//   }
+// `;
+
+// export const QUERY_MATCHUPS = gql`
+//   query matchups($_id: String) {
+//     matchups(_id: $_id) {
+//       _id
+//       tech1
+//       tech2
+//       tech1_votes
+//       tech2_votes
+//     }
+//   }
+// `;
