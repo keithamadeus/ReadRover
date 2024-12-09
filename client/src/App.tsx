bc1ecefebccdd52f26e7a8ce5b28cb5930b008ff
Keith <keith.amadeus.williams@gmail.com>
2024-12-08 20:17:13 -0700

    removed files that were not serving a purpose, no errors, working on logining in still

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
-// Import the custom CSS file for styling
 import "./App.css";
-// Import Apollo Client libraries for connecting to the GraphQL server
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink, } from "@apollo/client";
-// Import utility for setting headers with authentication tokens
 import { setContext } from "@apollo/client/link/context";
-// Import Outlet from React Router for nested routes
 import { Outlet } from "react-router-dom";
-
-// Import the Navbar component
 import Navbar from "./components/Navbar";
 
 // Create an HTTP link to connect to the GraphQL API endpoint
@@ -17,7 +11,6 @@ const httpLink = createHttpLink({
 
 // Set the authorization headers for every request
 const authLink = setContext((_, { headers }) => {
-  // Retrieve the JWT token from local storage and set the authorization header
   const token = localStorage.getItem("id_token");
   return {
     headers: {
